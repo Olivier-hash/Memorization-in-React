2aaed20e62612e774d29aec30cc81b0c5fa7f482
olivier Rukundo <rr.olivier01@gmail.com>
2025-04-28 20:39:32 +0200

    use of useCallback imported in app.jsx

---
+++ my-react-app/src/Test.jsx
@@ -0,0 +1,9 @@
+import { useCallback } from "react";
+
+function App() {
+  const handleClick = useCallback(() => {
+    console.log("Clicked!");
+  }, []);
+
+  return <button onClick={handleClick}>Click Me</button>;
+}
